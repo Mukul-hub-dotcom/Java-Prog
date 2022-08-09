f106a5b2c7322c970e6e1ce19e8a66f85a6aab8d
Mukul <ramdevmukul800@gmail.com>
2022-08-09 14:33:52 +0530

    updated

---
+++ javaVariables.java
@@ -0,0 +1,37 @@
+public class javaVariables{
+    public static void main(String args[]){
+        int a=10;
+        int b=5;
+        System.out.println(a);
+        System.out.println(b);
+        String name="Tony Stark";
+        System.out.println(name);
+        a=b;
+        System.out.println(a);
+        byte d=8;
+        System.out.println(d);
+        char ch='a';
+        System.out.println(ch); 
+        boolean e=true;
+        System.out.print(e);
+        // float price=10.5;
+        // int g=25;
+        // double use in large val. of float
+        // long for greater value of int
+        // short and byte used very less
+        // 8 datatypes in java
+        // datatype->Memory size
+        // byte=1 byte  [-128,128]range
+        // short =2byte
+
+        // char=2 byte
+        // boolean=1 byte
+        // int=4byte[-2billion,2bil] w/o decimal
+        // long =8byte
+        // float =4
+
+        // double=8
+        
+    }
+
+}
