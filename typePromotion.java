1ce5034470ba60dcbda41278fb30d219d5f687fc
Mukul <ramdevmukul800@gmail.com>
2022-08-11 01:37:03 +0530

    changed

---
+++ typePromotion.java
@@ -0,0 +1,13 @@
+public class typePromotion {
+// type promotion happens automatically while executing expressions
+
+    public static void main(String[] args) {
+        char a='a';
+        char b='b';
+        System.out.println(a);
+        System.out.println((int)(a));
+        System.out.println((int)(b));
+        System.out.println(b-a);
+
+    }
+}
