d7215a1a44dfd8aaf3bf8cb82addc8db168bcea0
Mukul <ramdevmukul800@gmail.com>
2022-08-11 01:37:39 +0530

    changed

--- typeCasting.java
+++ typeCasting.java
@@ -4,7 +4,7 @@ public class typeCasting {
     // type conversion happens only when destination is broader than source
     // byte<short<int<float<long<double
     // type conversion is also called widening || impicit conversion
-    // lossy conversion happens against type conversion
+    // lossy conversion happens against rule of type conversion which is not sensible at all we done it via typecasting
     public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     float number=sc.nextInt();
--- typePromotion.java
+++ typePromotion.java
@@ -1,13 +1,42 @@
 public class typePromotion {
-// type promotion happens automatically while executing expressions
+// type promotion happens only automatically in java while executing expressions
 
     public static void main(String[] args) {
         char a='a';
         char b='b';
+        // char c=a-b;
+        // lossy conversion from int to char msg displayed
         System.out.println(a);
         System.out.println((int)(a));
         System.out.println((int)(b));
         System.out.println(b-a);
+        char c='c';
+        byte bt=(byte)(a+b+c);
+        // here we do typecasting for lossyconversion ie int to byte change
+        System.out.println(bt);
+        
+
+        int g=10;
+        float h=20.5f;
+        long i=25;
+        double j=30;
+        double ans=g+h+i+j;
+        System.out.println(ans);
+
+     byte k=5;
+     k=(byte)(k*2);
+     System.out.println(k);
 
     }
+
 }
+// typePromotion 2 rules=>
+// if one operand is long,float or double then whole expression is converted to long float or double
+// java automatically converted to each byte short or char operand to int to evaluate an expression
+
+
+
+
+
+
+
