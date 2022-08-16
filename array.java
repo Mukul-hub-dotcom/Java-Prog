c97d8ffadb6ad28688b79e4b9dc18cf80726bc31
Mukul <ramdevmukul800@gmail.com>
2022-08-16 16:05:03 +0530

    done

---
+++ array.java
@@ -0,0 +1,46 @@
+public class array {
+    // array has ele of same datatype and indexing starts from zero
+    public static void update(int marks[],int notChange){      
+        notChange=89;
+        // System.out.println(notChange);
+        for(int i=0;i<marks.length;i++){
+            marks[i]+=1;
+        }
+    }
+
+    public static int linearSearch(int marks[],int key){
+        for(int i=0;i<marks.length;i++){
+            if(marks[i]==key){
+                return(i);
+            }
+            
+        }return -1;
+
+    }
+    public static void main(String[] args) {
+        // int marks[]=new int[50];
+        
+        // int numbers[]={1,2,3};
+
+        // marks[0]=99;
+        // marks[1]=99;
+        // System.out.println("Physics marks = "+marks[0]);
+        // System.out.print("Physics Rank = "+numbers[0]);
+
+        int marks[]={91,79,98,99,100};
+        int key=79;
+
+        System.out.println(linearSearch(marks,key));
+
+        int notChange=78;
+        update(marks,notChange);
+        // here we'll observe arr getting changed but int alone not updated as arr is call by reference and int notchange was call by value
+       System.out.println(notChange);
+        for(int i=0;i<marks.length;i++){
+            System.out.print(marks[i]+" ");
+        }
+        System.out.println();
+       
+    }
+
+}
