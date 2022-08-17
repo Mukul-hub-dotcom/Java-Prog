9ce1331e636fd55eebe7109aa3788f57c207a792
Mukul <ramdevmukul800@gmail.com>
2022-08-17 23:08:42 +0530

    done

--- arrayDsa.java
+++ arrayDsa.java
@@ -1,4 +1,4 @@
-public class dsa {
+public class array2 {
     // Linear Search
     public static int linearSearch(int marks[],int key){
         for(int i=0;i<marks.length;i++){
@@ -88,8 +88,74 @@ public static void subArray(int number[]){
     }System.out.println("total no. of subarray is = "+ts);
     // total no. of sub array is (n*(n+1))/2
 }
+// max sub arrray sum
+public static void maxSubarray(int number[]){
+    int max=Integer.MIN_VALUE;
+    int curr=0;
+
+    for(int i=0;i<number.length;i++){
+        // i will be starting and j will be end point and k for traversing
+        for(int j=i;j<number.length;j++){
+            curr=0;
+            // to make cur 0 for new sum
+
+            for (int k=i;k<=j;k++){
+                curr+=number[k];          
 
 
+            }
+            System.out.println(curr);
+            if(curr>max){
+                max=curr;
+            }            
+            
+        }
+    }
+    System.out.println("max Sum = "+max);    
+}
+
+// optimised find max sum of sub arr
+public static void maxSub2(int number[]){
+    int prefix[]=new int[number.length];
+    // size of prefix
+    // calc prefic arr
+    prefix[0]=number[0];
+    for(int i=1;i<prefix.length;i++){
+        prefix[i]=prefix[i-1]+number[i];
+    }
+    int max=Integer.MIN_VALUE;
+    int curr=0;
+    for(int i=0;i<number.length;i++){
+        // i will be starting and j will be end point 
+        for(int j=i;j<number.length;j++){
+            curr = i==0?prefix[j]:prefix[j]-prefix[i-1];
+            // to calc cur acc to prefix[]
+
+            if(curr>max){
+                max=curr;
+            }        
+            
+        }
+    }
+    System.out.println("max Sum = "+max);    
+}
+
+// jhaa final sum neg aay wha zero le k aage bde kedan
+// ye poore neg array k liye dikkat dega
+public static void kedanAlgo(int number[]){
+    // cs current sum ms max sum
+    int ms=Integer.MIN_VALUE;
+    int cs=0;
+    for(int i=0;i<number.length;i++){
+        cs=cs+number[i];
+        if(cs<0){
+            cs=0;
+        }
+        ms = Math.max(cs,ms);
+    }
+    
+    System.out.println("our max Sum = "+ms);    
+}
     public static void main(String[] args) {
         // int marks[]={97,45,79,47,102};
         // int key=85;
@@ -109,8 +175,20 @@ public static void subArray(int number[]){
 
         // pairing(number);
 
-        subArray(number);
+        // subArray(number);
+        maxSubarray(number);
+// {sum of max subarr}
+maxSub2(number);
+// {optimised approach to calc max sub arr}
 
+kedanAlgo(number);
+// for finding max sum of sub arr
+// it tells to take zero inspite of fully negative and move on
+// like ++,bigger+ small neg accept 
+// buut bigger neg reject
+
+
+// 1.linear search    2. find position 3. binary,position,reverse,subarr,max sub arr (3type)
 
 
     }
