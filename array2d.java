8018a8943996f426c98aa250aa09bc7a6ad188a9
Mukul <65118538+Mukul-hub-dotcom@users.noreply.github.com>
2022-12-07 22:52:13 +0530

    doin

--- array2d.java
+++ array2d.java
@@ -16,7 +16,7 @@ public class array2d {
         for (int i=0;i<n;i++){
             for(int j=0;j<m;j++){
               System.out.print( matrix[i][j]+" ");
-            System.out.println("");
+            
             }System.out.println();
         }
     }
