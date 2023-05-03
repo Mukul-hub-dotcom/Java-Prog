ce772e2fa51eed533d8a2e3a75ac4a775dbb2023
Mukul <65118538+Mukul-hub-dotcom@users.noreply.github.com>
2023-05-03 18:48:25 +0530

    another question

--- rain.java
+++ rain.java
@@ -24,8 +24,27 @@ public class rain{
         return trappedWater;
 
     }
+
+    public static int bysell(int price[]){
+        // auxillary profit array
+        int buyPrice=Integer.MAX_VALUE;
+        int maxProfit=0;
+        for(int i=0;i<price.length;i++){
+            if(buyPrice<price[i]){
+                int profit=price[i]-buyPrice;
+                maxProfit=Math.max(maxProfit,profit);
+            }
+            else{
+                buyPrice=price[i];
+            }
+            
+        }
+return maxProfit;
+    }
     public static void main(String args[]){
         int height[]={4,2,0,6,3,2,5};
         System.out.println(trappedRainWater(height));
+        int price[]={7,1,5,3,6,4};
+        System.out.println(bysell(price));
     }
 }
